8ba0c5f38ada2923eed5d158335e958b8cfc453c
Cesar-64bit <cesar.1ng.1nf@gmail.com>
2022-07-20 21:16:01 -0600

    Se agrego el icono en el login

--- DeMar/src/login/LoginGraficos.java
+++ DeMar/src/login/LoginGraficos.java
@@ -6,13 +6,8 @@ import javax.swing.JPasswordField;
 // import javax.swing.JTextArea;
 import javax.swing.JTextField;
 import javax.swing.SwingConstants;
-import javax.swing.border.BevelBorder;
-import javax.swing.plaf.FontUIResource;
-import javax.swing.text.AttributeSet.ColorAttribute;
-import javax.swing.ButtonGroup;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
-import javax.swing.JCheckBox;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -23,23 +18,21 @@ public class LoginGraficos extends JFrame {
   private static final long serialVersionUID = 1L;
 
   // Declaración Objetos Gráficos
-  private JPanel pLogin, pFondo;
-  private JLabel lblNombreEmpresa, lEslogan, lTituloLogin, lNotificaciones;
+  private JPanel pLogin, pFondo, pUsuario, pContraseña;
+  private JLabel lblUsuario, lblContraseña;
   private JTextField txtNombreUsuario;
   private JPasswordField txtClaveUsuario;
   private JComboBox<String> cbxTipoUsuario;
-  private JButton bEntrar, bCerrar, bRegistrarse, bOpcion1, bOpcion2, bOpcion3;
-  private JCheckBox checkSi, checkNo;
-  private ButtonGroup grupo;
-  private ImageIcon iFondo;
-  private JLabel lFondo;
+  private JButton btnIngresar;
+  private ImageIcon iFondo, iIcono;
+  private JLabel lFondo, lIcono;
   private Font fontNombreEmpresa;
 
   // private JRadioButton rbOpcion1, rbOpcion2;
   // private JTextArea taSugerencias;
 
   public LoginGraficos() {
-    super("Login Usuario");
+    super("De Mar");
 
     /* Panel para contener el fondo */
     pFondo = new JPanel();
@@ -68,48 +61,97 @@ public class LoginGraficos extends JFrame {
     this.add(pLogin);
     lFondo.add(pLogin);
 
+    /* Dibujar Icono */ 
+
+    iIcono = new ImageIcon("DeMar/resources/images/AtunIcono.jpg");
+
+    lIcono = new JLabel();
+    lIcono.setSize(128,128);
+    lIcono.setLocation((pLogin.getWidth() - lIcono.getWidth()) / 2, 20);
+    lIcono.setIcon(iIcono);
+    pLogin.add(lIcono);
+
+
+    /* Panel separador */
+    pUsuario = new JPanel();
+    pUsuario.setSize(260,1);
+    pUsuario.setLocation((pLogin.getWidth() - pUsuario.getWidth()) / 2, 215);
+    pUsuario.setBackground(Color.BLACK);
+    this.add(pUsuario);
+    pLogin.add(pUsuario);
+
+    pContraseña = new JPanel();
+    pContraseña.setSize(260,1);
+    pContraseña.setLocation((pLogin.getWidth() - pUsuario.getWidth()) / 2, 295);
+    pContraseña.setBackground(Color.BLACK);
+    this.add(pContraseña);
+    pLogin.add(pContraseña);
+
     /* Fuente del título*/
     fontNombreEmpresa = new Font("Forte", Font.PLAIN, 30);
 
     /* Labels */
-    lblNombreEmpresa = new JLabel("DE MAR");
+  /*   lblNombreEmpresa = new JLabel("DE MAR");
     lblNombreEmpresa.setSize(150,30);
     lblNombreEmpresa.setLocation((pLogin.getWidth() - lblNombreEmpresa.getWidth()) / 2, 60);
     lblNombreEmpresa.setForeground(Color.BLACK);
     lblNombreEmpresa.setHorizontalAlignment(SwingConstants.CENTER);
     lblNombreEmpresa.setFont(fontNombreEmpresa);
-    pLogin.add(lblNombreEmpresa);
+    pLogin.add(lblNombreEmpresa);*/
 
+
+    lblUsuario = new JLabel("Usuario");
+    lblUsuario.setSize(150, 30);
+    lblUsuario.setLocation(50, 145);
+    lblUsuario.setForeground(Color.DARK_GRAY);
+    pLogin.add(lblUsuario);
+
+    lblContraseña = new JLabel("Contraseña");
+    lblContraseña.setSize(150, 30);
+    lblContraseña.setLocation(50, 225);
+    lblContraseña.setForeground(Color.DARK_GRAY);
+    pLogin.add(lblContraseña);
 
     /* Caja de texto */
-    txtNombreUsuario = new JTextField("Nombre de usuario"); 
+    txtNombreUsuario = new JTextField("Ingresar usuario"); 
     txtNombreUsuario.setSize(260,40);
-    txtNombreUsuario.setLocation((pLogin.getWidth() - txtNombreUsuario.getWidth()) / 2, 120);
-    txtNombreUsuario.setForeground(Color.DARK_GRAY);
+    txtNombreUsuario.setLocation((pLogin.getWidth() - txtNombreUsuario.getWidth()) / 2,175);
+    txtNombreUsuario.setForeground(Color.GRAY);
     txtNombreUsuario.setBackground(Color.WHITE);
-    txtNombreUsuario.setCaretColor(Color.BLUE);
+    txtNombreUsuario.setCaretColor(Color.BLACK);
+    txtNombreUsuario.setBorder(null);
     txtNombreUsuario.setHorizontalAlignment(SwingConstants.CENTER);
     pLogin.add(txtNombreUsuario);
 
     /* Caja de texto con contraseña */
     txtClaveUsuario = new JPasswordField("Contraseña");
     txtClaveUsuario.setSize(260, 40);
-    txtClaveUsuario.setLocation((pLogin.getWidth() - txtClaveUsuario.getWidth()) / 2, 200);
+    txtClaveUsuario.setLocation((pLogin.getWidth() - txtClaveUsuario.getWidth()) / 2, 250);
     txtClaveUsuario.setForeground(Color.DARK_GRAY);
-    txtClaveUsuario.setCaretColor(Color.BLUE);
+    txtClaveUsuario.setCaretColor(Color.BLACK);
+    txtClaveUsuario.setBorder(null);
     txtClaveUsuario.setHorizontalAlignment(SwingConstants.CENTER);
     pLogin.add(txtClaveUsuario);
 
-    /* Combo box */
+    /* ComboBox */
     cbxTipoUsuario = new JComboBox<>();
-    cbxTipoUsuario.addItem("Gerente");
+    cbxTipoUsuario.addItem("Recepcionista");
     cbxTipoUsuario.addItem("Administrador");
     cbxTipoUsuario.setSize(220,30);
-    cbxTipoUsuario.setLocation((pLogin.getWidth() - cbxTipoUsuario.getWidth()) / 2, 280);
+    cbxTipoUsuario.setLocation((pLogin.getWidth() - cbxTipoUsuario.getWidth()) / 2, 315);
     cbxTipoUsuario.setForeground(Color.DARK_GRAY);
     cbxTipoUsuario.setBackground(Color.WHITE);
     ((JLabel) cbxTipoUsuario.getRenderer()).setHorizontalAlignment(SwingConstants.CENTER);
     pLogin.add(cbxTipoUsuario);
+
+    /* Boton ingresar */
+    btnIngresar = new JButton("Ingresar");
+    btnIngresar.setSize(140, 40);
+    btnIngresar.setLocation((pLogin.getWidth() - btnIngresar.getWidth()) / 2, 375);
+    btnIngresar.setBackground(Color.WHITE);
+    btnIngresar.setForeground(Color.DARK_GRAY);
+    btnIngresar.setFocusable(false);
+    pLogin.add(btnIngresar);
 
     setLayout(null);
     setDefaultCloseOperation(EXIT_ON_CLOSE);
