6c03570300f8b3b996c140f4fb1abe398182a5ff
Cesar-64bit <cesar.1ng.1nf@gmail.com>
2022-07-21 19:46:47 -0600

    Prueba 1

--- DeMar/src/menuPrincipal/MenuPrincipalVista.java
+++ DeMar/src/menuPrincipal/MenuPrincipalVista.java
@@ -3,10 +3,11 @@ package DeMar.src.menuPrincipal;
 import javax.swing.JFrame;
 
 public class MenuPrincipalVista extends JFrame{
+
     public MenuPrincipalVista() {
         super("Menu Principal");
-        
-        setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+     //   setDefaultCloseOperation(EXIT_ON_CLOSE); // Esto sirve para cerrar todo el programa
         setSize(1200, 600);
         setLocationRelativeTo(this);
         setLayout(null);
