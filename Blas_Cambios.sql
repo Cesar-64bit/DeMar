6c5c89feff5328996d8785c81a657a63baa006f5
Cesar-64bit <cesar.1ng.1nf@gmail.com>
2022-07-28 14:41:46 -0600

    Casi terminada la ventana de Areas

--- Blas_Cambios.sql
+++ Blas_Cambios.sql
@@ -65,19 +65,3 @@ call demar.buscarUnPedido('1');
 call demar.seleccionarPedidos();
 call demar.seleccionarPedidosPen('0');
 call demar.seleccionarPedidosPen('1');
-
-
--- CESAR_26/07/2022
---- Procedimiento para MOSTRAR TODAS LAS AREAS
-CREATE PROCEDURE seleccionarAreas();
-SELECT *FROM areas;
-
---- Procedimiento para MOSTRAR UN AREA CON SU ID
-CREATE PROCEDURE buscarUnArea(IN idArea VARCHAR(11));
-SELECT *FROM areas WHERE id = idArea;
-
---------------------------------------------------------
-
---- PRUEBAS
-call seleccionarAreas();
-call buscarUnArea('3');
